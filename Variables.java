9e8765c090ba3d873af1f408f8ec860f8940cba3
AndreyBurov0702 <129365108+AndreyBurov0702@users.noreply.github.com>
2023-03-30 20:40:59 +0500

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myInt = 702; // 32bit
+        short myShort = 1702; // 16bit
+        long myLong = 17020305; // 64bit
+
+        double myDouble = 702.35; // 64bit
+        float myFloat = 1702.35f; // 32bit
+
+        char c = 'a';
+        boolean b = true;
+
+        byte myByte = 100; // 8bit -128-127
+        System.out.println(myShort);
+    }
+}
